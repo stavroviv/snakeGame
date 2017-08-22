37b8c519215a0dedb091761ead7cb46b92a42369
istavrov <istavrov@100.65.53.34>
2017-08-22 23:17:12 +0400

    coment remove

---
+++ SnakeGame/src/SnakeGame/snakeApp.java
@@ -0,0 +1,16 @@
+package SnakeGame;
+
+import javax.swing.JLabel;
+
+public class snakeApp {
+	
+	public static mainForm snakeGame;
+	
+	public static void main(String[] args) {
+		
+		snakeGame = new mainForm();
+		snakeGame.setVisible(true);
+		
+	}
+
+}
